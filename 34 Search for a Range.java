d83d115bf2eafc54be7ab2ba30caf3541d1e344c
jiaowcg88 <jw4321@nyu.edu>
2017-05-30 15:43:20 -0400

    34 Search for a range
    using a binary search algorithm.

---
+++ 34 Search for a Range.java
@@ -0,0 +1,51 @@
+/**
+Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].
+
+*/
+
+/**
+since requirement of runtime is O(logn), we think that we need to use binary search method to solve this problem
+search the middle element, if the middle value is less than target, then we need to search through the right side of the current middle value, if the middle value is greater then target, then we need to search through the left side of the current middle value. the last case is that if the middle value equals the target. then we have to search the right and left side of the current middle value. 
+
+*/
+
+public class Solution {
+	public int[] searchRange(int[] nums, int target){
+		int[] result = new int[]{-1,-1};  // pay attendtion here
+		int left = 0;
+		int right = nums.length -1;
+		while (left <= right){
+			int mid = left + (right - left)/2;
+			// if the middle value equals target
+			if (nums[mid] == target){
+				int i = mid;
+				while (i+1 < nums.length && nums[i+1] == target) {// skip the repeating value to find the first different one
+					i++;
+				}
+				result[1] = i;
+				int j = mid;
+				while (j-1 >= 0 && nums[j-1] == target){ // skip the repeating value to find the first different one
+					j--;
+				}
+				result[0] = j;
+				return result;
+			} else if (nums[mid] < target){	
+				left = mid+1;
+
+			} else {
+				right = mid-1;
+			}
+
+		}
+		return result;
+
+	}
+}
