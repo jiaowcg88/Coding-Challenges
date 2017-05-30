2a743310c4584d60a1b4c2901e9341c5479bf455
jiaowcg88 <jw4321@nyu.edu>
2017-05-30 14:52:49 -0400

    136 Single Number
    using XOR bit manipulation to solve the problem

---
+++ 136 Single Number.java
@@ -0,0 +1,19 @@
+/**
+Given an array of integers, every element appears twice except for one. Find that single one.
+
+*/
+
+/**
+idea: using XOR to solve, if two element is same, XOR is 0, else is 1. 
+
+*/
+
+public class Solution {
+	public int singleNumber(int[] nums){
+		int result = nums[0];
+		for (int i = 1; i<nums.length; i++){
+			result ^= nums[i];
+		}
+		return result;
+	}
+}
