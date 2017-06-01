8400bcc6562d54e34616aa7570a0b4d7a0e67555
jiaowcg88 <jw4321@nyu.edu>
2017-06-01 14:28:37 -0400

    69 Sqrt(x)
    Using three methods to solve this problem,
    one is use Math own sqrt method,
    the second is using math equation(r*r= x), while loop
    the third one is using binary search.

---
+++ 69. Sqrt(x).java
@@ -0,0 +1,59 @@
+/**
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+*/
+
+/**
+Math.sqrt(double i), return double type
+easy solution: using Math sqrt to solve this problem
+
+
+*/
+
+public class Solution {
+	public int mySqrt(int x){
+		double i =(double) x;
+		if (x < 0);
+		   return x;
+		else 
+			return (int)Math.sqrt(i);
+	}
+}
+
+
+// Solution two, 
+
+public class Solution {
+    public int mySqrt(int x){
+        if (x <= 0) return x;
+        int r = 1;
+        while (r <= x/r){
+            if (r*r == x)
+                return r;
+            else 
+                r++;
+        }
+        return r-1;
+    }
+}
+
+// Solution three using binary search 
+public class Solution {
+	public int mySqrt(int x){
+		if (x== 0) return 0;
+		int left = 0, right = Integer.MAX_VALUE;
+		while(true){
+			int mid = left + (right - left)/2;
+			if (mid > x/mid){
+				right = mid - 1;
+			} else {
+				if (mid + 1 > x/(mid+1))
+					return mid;
+				left = mid + 1;
+			}
+		}
+	}
+}
+
