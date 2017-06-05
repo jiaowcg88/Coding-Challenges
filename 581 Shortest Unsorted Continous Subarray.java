c25838e85a836e3782f4ebab2a66177926012c22
jiaowcg88 <jw4321@nyu.edu>
2017-06-05 12:08:27 -0400

    Update 581 Shortest Unsorted Continous Subarray.java

--- 581 Shortest Unsorted Continous Subarray.java
+++ 581 Shortest Unsorted Continous Subarray.java
@@ -33,3 +33,21 @@ public class Solution {
         
     }
 }
+
+
+// Solution two, O(n)
+//idea: using two variable start, end to keed tracking of minimum subarray A[start...end] which must be sorted for the entire array A 
+//to be sorted. if end  < start < 0 at the end of the for loop, then array is already fully sorted
+public class Solution {
+    public int findUnsortedSubarray(int[] nums){
+        int n = nums.length;
+        int start = -1, end = -2, min = nums[n-1], max = nums[0];
+        for (int i = 1; i<n; i++){
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n-1-i]);
+            if(nums[i] < max) end = i;
+            if (nums[n-1-i] > min) start = n-1-i;
+        }
+        return end - start +1;
+    }
+}
