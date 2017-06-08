7ad16f34e6a0a8613a0f26c7a47f9571554e4473
jiaowcg88 <jw4321@nyu.edu>
2017-06-08 16:04:29 -0400

    Update 69. Sqrt(x).java
    add another method of using binary search

--- 69. Sqrt(x).java
+++ 69. Sqrt(x).java
@@ -57,3 +57,22 @@ public class Solution {
 	}
 }
 
+// another method of using binary search
+public class Solution {
+	public int mySqrt(int x){
+		long start = 1, end = x;
+		while(start + 1 < end){
+			long mid = start + (end-start)/2;
+			if (mid*mid <= x){
+				start = mid;
+			}else {
+				end = mid;
+			}
+		}
+		if (end*end <= x)
+			return (int)end;
+        else
+		   return (int)start;
+		
+	}
+}
