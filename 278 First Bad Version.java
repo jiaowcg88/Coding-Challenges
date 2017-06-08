4354297cbbcf2b265323a06c3ccae04ee610b79b
jiaowcg88 <jw4321@nyu.edu>
2017-06-08 15:28:21 -0400

    278 First Bad Version
    using binary search to find the first position of bad version

---
+++ 278 First Bad Version.java
@@ -0,0 +1,35 @@
+/**
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+*/
+
+/**
+using binary search to solve. 
+find the first position of bad version 
+if we have two pointers, one pointer points to the beginning of the array, the other pointer points to the end of the array
+if the mid one is bad version, we need to check from the array [left, mid-1],
+else check [mid+1, right]
+
+
+*/
+
+public class Solution extends VersionControl {
+	public int firstBadVersion (int n){
+		int start = 1, end = n;
+		while (start + 1 < end){
+			int mid = start + (end - start)/2;
+			if (isBadVersion(mid)) {
+				end = mid;
+			}else {
+				start = mid;
+			}
+		}
+		if (isBadVersion(start))
+			return start;
+		return end;
+	}
+}
