abf1835a85417e9b6042cbfd82fc71d8192165d3
Jiao (Joanna) Wu <jw4321@nyu.edu>
2018-03-17 22:27:11 -0400

    Update 138 deepCopy.java

--- LinkedList/138 deepCopy.java
+++ LinkedList/138 deepCopy.java
@@ -1,1 +1,58 @@
 
+/*
+* Description:
+* A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+* Return a deep copy of the list.
+*/
+/*
+* step 1: make a copy of  a linked list
+* step 2: make the random pointer to the copy node:
+* step 3: restore the original linked list:
+* The time complexity is : Great solution, but it is not O(1) space, you can argue that no extra data structure is used, but it’s still O(n) space.
+*/
+
+public RandomListNode copyRandomList(RandomListNode head) {
+	RandomListNode iter = head, next;
+
+	// First round: make copy of each node,
+	// and link them together side-by-side in a single list.
+	while (iter != null) {
+		next = iter.next;
+
+		RandomListNode copy = new RandomListNode(iter.label);
+		iter.next = copy;
+		copy.next = next;
+
+		iter = next;
+	}
+
+	// Second round: assign random pointers for the copy nodes.
+	iter = head;
+	while (iter != null) {
+		if (iter.random != null) {
+			iter.next.random = iter.random.next;
+		}
+		iter = iter.next.next;
+	}
+
+	// Third round: restore the original list, and extract the copy list.
+	iter = head;
+	RandomListNode pseudoHead = new RandomListNode(0);
+	RandomListNode copy, copyIter = pseudoHead;
+
+	while (iter != null) {
+		next = iter.next.next;
+
+		// extract the copy
+		copy = iter.next;
+		copyIter.next = copy;
+		copyIter = copy;
+
+		// restore the original list
+		iter.next = next;
+
+		iter = next;
+	}
+
+	return pseudoHead.next;
+}
